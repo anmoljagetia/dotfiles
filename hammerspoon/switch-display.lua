aeee0f26a17bc3f1f3b98a5c43e86f2ad5467b17
Anmol Jagetia <anmol.j@media.net>
2017-11-13 21:23:15 +0530

    Updates the hammerspoon config for switching display

--- hammerspoon/switch-display.lua
+++ hammerspoon/switch-display.lua
@@ -22,6 +22,32 @@ function isInScreen(screen, win)
   return win:screen() == screen
 end
 
+-- Adds a circle around the mouse when swiching the display to show that the
+-- focus has shifted.
+local mouseCircle = nil
+local mouseCircleTimer = nil
+
+function mouseHighlight()
+    -- Delete an existing highlight if it exists
+    if mouseCircle then
+        mouseCircle:delete()
+        if mouseCircleTimer then
+            mouseCircleTimer:stop()
+        end
+    end
+    -- Get the current co-ordinates of the mouse pointer
+    mousepoint = hs.mouse.getAbsolutePosition ()
+    -- Prepare a big red circle around the mouse pointer
+    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
+    mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
+    mouseCircle:setFill(false)
+    mouseCircle:setStrokeWidth(5)
+    mouseCircle:show(0.5)
+
+    -- Set a timer to delete the circle after 1.5 second
+    mouseCircleTimer = hs.timer.doAfter(1.5, function() mouseCircle:delete() end)
+end
+
 -- Brings focus to the screen by setting focus on the front-most application in it.
 -- Also move the mouse cursor to the center of the screen. This is because
 -- Mission Control gestures & keyboard shortcuts are anchored, oddly, on where the
@@ -40,6 +66,7 @@ function focusScreen(screen, mouse)
   --local pt = geometry.rectMidPoint(screen:fullFrame())
   local pt = hs.geometry.rectMidPoint(mouse:fullFrame())
   hs.mouse.setAbsolutePosition(pt)
+  mouseHighlight()
 end
 
 -- Mouse switching logic taken and adapted from : https://gist.github.com/sthtodo/51710df48cf6110e758f
