13cc6b49ef1655e3d2b528d5c078e2c56f7f2f48
Anmol Jagetia <anmol.j@media.net>
2019-03-15 19:22:22 +0530

    Updates the hs-weather plugin to work with OpenWeatherMap API

--- hammerspoon/hs-weather/init.lua
+++ hammerspoon/hs-weather/init.lua
@@ -3,61 +3,74 @@ local m = {}
 local hammerDir = hs.fs.currentDir()
 local iconsDir = (hammerDir .. '/hs-weather/icons/')
 local configFile = (hammerDir .. '/hs-weather/config.json')
-local urlBase = 'https://query.yahooapis.com/v1/public/yql?q='
-local query = 'select item.title, item.condition from weather.forecast where \
-               woeid in (select woeid from geo.places(1) where text="'
+local urlBase = 'http://api.openweathermap.org/data/2.5/weather?'
+local query = '&APPID=9d58c913dd7e0a32801130b9ebcfe9e0&units=metric&q='
 
 -- https://developer.yahoo.com/weather/archive.html#codes
 -- icons by RNS, Freepik, Vectors Market, Yannick at http://www.flaticon.com
 local weatherSymbols = {
-  [0] = (iconsDir .. 'tornado.png'),      -- tornado
-  [1] = (iconsDir .. 'storm.png'),        -- tropical storm
-  [2] = (iconsDir .. 'tornado.png'),      -- hurricane
-  [3] = (iconsDir .. 'storm-5.png'),      -- severe thunderstorms
-  [4] = (iconsDir .. 'storm-4.png'),      -- thunderstorms
-  [5] = (iconsDir .. 'sleet.png'),        -- mixed rain and snow
-  [6] = (iconsDir .. 'sleet.png'),        -- mixed rain and sleet
-  [7] = (iconsDir .. 'sleet.png'),        -- mixed snow and sleet
-  [8] = (iconsDir .. 'drizzle.png'),      -- freezing drizzle
-  [9] = (iconsDir .. 'drizzle.png'),      -- drizzle
-  [10] = (iconsDir .. 'drizzle.png'),     -- freezing rain
-  [11] = (iconsDir .. 'rain-1.png'),      -- showers
-  [12] = (iconsDir .. 'rain-1.png'),      -- showers
-  [13] = (iconsDir .. 'snowflake.png'),   -- snow flurries
-  [14] = (iconsDir .. 'snowflake.png'),   -- light snow showers
-  [15] = (iconsDir .. 'snowflake.png'),   -- blowing snow
-  [16] = (iconsDir .. 'snowflake.png'),   -- snow
-  [17] = (iconsDir .. 'hail.png'),        -- hail
-  [18] = (iconsDir .. 'sleet.png'),       -- sleet
-  [19] = (iconsDir .. 'haze.png'),        -- dust
-  [20] = (iconsDir .. 'mist.png'),        -- foggy
-  [21] = (iconsDir .. 'haze.png'),        -- haze
-  [22] = (iconsDir .. 'mist.png'),        -- smoky
-  [23] = (iconsDir .. 'wind-1.png'),      -- blustery
-  [24] = (iconsDir .. 'windy-1.png'),     -- windy
-  [25] = (iconsDir .. 'cold.png'),        -- cold
-  [26] = (iconsDir .. 'clouds.png'),      -- cloudy
-  [27] = (iconsDir .. 'night.png'),       -- mostly cloudy (night)
-  [28] = (iconsDir .. 'cloudy.png'),      -- mostly cloudy (day)
-  [29] = (iconsDir .. 'cloudy-4.png'),    -- partly cloudy (night)
-  [30] = (iconsDir .. 'cloudy-5.png'),    -- partly cloudy (day)
-  [31] = (iconsDir .. 'moon-2.png'),      -- clear (night)
-  [32] = (iconsDir .. 'sun-1.png'),       -- sunny
-  [33] = (iconsDir .. 'night-2.png'),     -- fair (night)
-  [34] = (iconsDir .. 'cloudy-1.png'),    -- fair (day)
-  [35] = (iconsDir .. 'hail.png'),        -- mixed rain and hail
-  [36] = (iconsDir .. 'temperature.png'), -- hot
-  [37] = (iconsDir .. 'storm-4.png'),     -- isolated thunderstorms
-  [38] = (iconsDir .. 'storm-2.png'),     -- scattered thunderstorms
-  [39] = (iconsDir .. 'rain-3.png'),      -- scattered thunderstorms
-  [40] = (iconsDir .. 'rain-6.png'),      -- scattered showers
-  [41] = (iconsDir .. 'snowflake.png'),   -- heavy snow
-  [42] = (iconsDir .. 'snowflake.png'),   -- scattered snow showers
-  [43] = (iconsDir .. 'snowflake.png'),   -- heavy snow
-  [44] = (iconsDir .. 'cloudy.png'),      -- party cloudy
-  [45] = (iconsDir .. 'storm.png'),       -- thundershowers
-  [46] = (iconsDir .. 'snowflake.png'),   -- snow showers
-  [47] = (iconsDir .. 'lightning.png'),   -- isolated thundershowers
+  -- Group 2xx : Thunderstorm 
+  [200] = (iconsDir .. 'storm.png'),      -- thunderstorm with light rain
+  [201] = (iconsDir .. 'storm.png'),      -- thunderstorm with rain
+  [202] = (iconsDir .. 'storm-5.png'),      -- thunderstorm with heavy rain
+  [210] = (iconsDir .. 'storm-1.png'),      -- light thunderstorm
+  [211] = (iconsDir .. 'storm-2.png'),      -- thunderstorm
+  [212] = (iconsDir .. 'storm-3.png'),      -- heavy thunderstorm
+  [221] = (iconsDir .. 'storm-4.png'),      -- ragged thunderstorm
+  [230] = (iconsDir .. 'storm.png'),      -- thunderstorm with light drizzle
+  [231] = (iconsDir .. 'storm.png'),      -- thunderstorm with drizzle
+  [232] = (iconsDir .. 'storm-5.png'),      -- thunderstorm with heavy drizzle
+  -- Group 3xx : Drizzle 
+  [300] = (iconsDir .. '.png'),      -- light intensity drizzle
+  [301] = (iconsDir .. 'drizzle.png'),      -- drizzle
+  [302] = (iconsDir .. 'rain-1.png'),      -- heavy intensity drizzle
+  [310] = (iconsDir .. 'rain-1.png'),      -- light intensity drizzle with rain
+  [311] = (iconsDir .. 'rain.png'),      -- drizzle rain
+  [312] = (iconsDir .. 'rain-1.png'),      -- heavy intensity drizzle rain
+  [313] = (iconsDir .. 'rain.png'),      -- shower rain and drizzle
+  [314] = (iconsDir .. 'rain-1.png'),      -- heavy shower rain and drizzle
+  [321] = (iconsDir .. 'drizzle.png'),      -- shower drizzle
+  -- Group 5xx : Rains 
+  [500] = (iconsDir .. 'rain-6.png'),      -- light rain 
+  [501] = (iconsDir .. 'rain-3.png'),      -- moderate rain
+  [502] = (iconsDir .. 'rain-1.png'),      -- heavy intensity rain
+  [503] = (iconsDir .. 'rain-1.png'),      -- very heavy rain
+  [504] = (iconsDir .. 'rain-7.png'),      -- extreme rain
+  [511] = (iconsDir .. 'rain-8.png'),      -- freezing rain
+  [520] = (iconsDir .. 'rain-6.png'),      -- light intensity shower rain
+  [521] = (iconsDir .. 'rain-1.png'),      -- shower rain
+  [522] = (iconsDir .. 'rain.png'),      -- heavy intensity shower rain
+  [531] = (iconsDir .. 'rain-4.png'),      -- ragged shower rain
+  -- Group 6xx : Snow 
+  [600] = (iconsDir .. 'snowflake.png'),      -- light snow
+  [601] = (iconsDir .. 'snowflake.png'),      -- snow
+  [602] = (iconsDir .. 'snowflake-1.png'),      -- heavy snow
+  [611] = (iconsDir .. 'sleet.png'),      -- sleet
+  [612] = (iconsDir .. 'sleet.png'),      -- light shower sleet
+  [613] = (iconsDir .. 'sleet.png'),      -- shower sleet
+  [615] = (iconsDir .. 'rain-8.png'),      -- light rain and snow
+  [616] = (iconsDir .. 'hail.png'),      -- rain and snow
+  [620] = (iconsDir .. 'rain-8.png'),      -- light shower snow
+  [621] = (iconsDir .. 'rain-8.png'),      -- shower snow
+  [622] = (iconsDir .. 'rain-8.png'),      -- heavy shower snow
+  -- Group 7xx : Atmosphere 
+  [701] = (iconsDir .. 'mist.png'),       -- mist 
+  [711] = (iconsDir .. 'wind-1.png'),       -- smoke
+  [721] = (iconsDir .. 'haze.png'),      -- haze
+  [731] = (iconsDir .. 'tornado.png'),      -- sand, dust whirls
+  [741] = (iconsDir .. 'dawn-1.png'),      -- fog
+  [751] = (iconsDir .. 'windy-3.png'),      -- sand
+  [761] = (iconsDir .. 'windy-2.png'),      -- dust
+  [762] = (iconsDir .. 'windy.png'),      -- volcanic ash
+  [771] = (iconsDir .. 'night.png'),      -- squalls
+  [781] = (iconsDir .. 'tornado.png'),      -- tornado
+  -- Group 8xx : Clear and Cloudy
+  [800] = (iconsDir .. 'sun-4.png'),      -- clear sky
+  [801] = (iconsDir .. 'cloud.png'),      -- few clouds
+  [802] = (iconsDir .. 'cloud-1.png'),      -- scattered clouds
+  [803] = (iconsDir .. 'cloudy-3.png'),      -- broken clouds
+  [804] = (iconsDir .. 'clouds.png'),      -- overcast clouds
+  -- Group N/A
   [3200] = (iconsDir .. 'na.png')         -- not available
 }
 
@@ -81,39 +94,19 @@ local function setWeatherIcon(app, code)
   end
 end
 
-local function toCelsius(f)
-  return (f - 32) * 5 / 9
+local function setWeatherTitle(app, temp)
+    app:setTitle(temp .. '°C')
 end
 
-local function setWeatherTitle(app, unitSys, temp)
-  if unitSys == 'C' then
-    local tempCelsius = toCelsius(temp)
-    local tempRounded = math.floor(tempCelsius * 10 + 0.5) / 10
-    app:setTitle(tempRounded .. ' °C')
-  else
-    app:setTitle(temp .. ' °F')
-  end
-end
-
-local function urlencode(str)
-  if (str) then
-    str = string.gsub (str, "\n", "\r\n")
-    str = string.gsub (str, "([^%w ])",
-      function (c) return string.format ("%%%02X", string.byte(c)) end)
-    str = string.gsub (str, " ", "+")
-  end
-  return str
-end
 
 local function getWeather(location)
-  local weatherEndpoint = (
-    urlBase .. urlencode(query .. location .. '")') .. '&format=json')
+  local weatherEndpoint = (urlBase .. query .. location)
+  print(weatherWndpoint)
   return hs.http.get(weatherEndpoint)
 end
 
 local function setWeatherForLocation(location, unitSys)
-  local weatherEndpoint = (
-    urlBase .. urlencode(query .. location .. '")') .. '&format=json')
+  local weatherEndpoint = (urlBase .. query .. location)
   hs.http.asyncGet(weatherEndpoint, nil,
     function(code, body, table)
       if code ~= 200 then
@@ -121,18 +114,18 @@ local function setWeatherForLocation(location, unitSys)
       else
         print('-- hs-weather: Weather for ' .. location .. ': ' .. body)
         local response = hs.json.decode(body)
-        if response.query.results == nil then
+        if response == nil then
           if m.weatherApp:title() == '' then
             setWeatherIcon(m.weatherApp, 3200)
           end
         else
-          local temp = response.query.results.channel.item.condition.temp
-          local code = tonumber(response.query.results.channel.item.condition.code)
-          local condition = response.query.results.channel.item.condition.text
-          local title = response.query.results.channel.item.title
+          local temp = response.main.temp
+          local code = tonumber(response.weather[1].id)
+          local description = response.weather[1].description
+          local title = response.weather[1].main
           setWeatherIcon(m.weatherApp, code)
-          setWeatherTitle(m.weatherApp, unitSys, temp)
-          m.weatherApp:setTooltip((title .. '\n' .. 'Condition: ' .. condition))
+          setWeatherTitle(m.weatherApp, temp)
+          m.weatherApp:setTooltip((title .. '\n' .. 'Description: ' .. description))
         end
       end
     end
@@ -170,7 +163,7 @@ m.start = function(cfg)
   -- defaults if not set
   m.config.refresh = m.config.refresh or 300
   m.config.units = m.config.units or 'C'
-  m.config.location = m.config.location or 'Berlin, DE'
+  m.config.location = m.config.location or 'Mumbai,IN'
 
   m.weatherApp = hs.menubar.new()
   setWeather()
